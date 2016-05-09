0b14266cc03876c9ab04e0ccf050717f32a16d48
Ian C <fortytw2@gmail.com>
2016-05-09 16:27:56 -0500

    set up initial daemon

--- cmd/kiasu/kiasu.go
+++ cmd/kiasu/kiasu.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"gopkg.in/inconshreveable/log15.v2"
+	"net/http"
+
+	"github.com/fortytw2/kiasu/vendor/gopkg.in/inconshreveable/log15.v2"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	l := log15.New()
-
 	l.Info("starting kiasu")
+
+	r := httprouter.New()
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		l.Crit("could not start kiasu", "error", err.Error())
+	}
 }
