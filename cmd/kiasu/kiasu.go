b9635aeb83c6da295d521cb45b806f18b777d6f1
Ian C <fortytw2@gmail.com>
2016-06-05 08:58:48 -0500

    add basic HTML renderer

--- cmd/kiasu/kiasu.go
+++ cmd/kiasu/kiasu.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/fortytw2/kiasu/api"
+	"github.com/fortytw2/kiasu/web"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/inconshreveable/log15.v2"
 )
@@ -21,10 +22,11 @@ func main() {
 
 	r := httprouter.New()
 
+	r.GET("/feed", web.ListFeeds(nil, nil))
+
 	// r.GET("/v1/extractors", api.Extractors)
 	// r.GET("/v1/feeds", api.Feeds)
-	r.GET("/v1/feed/", api.ListFeeds(nil, nil))
-
+	r.GET("/api/v1/feed/", api.ListFeeds(nil, nil))
 	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		l.Crit("could not start kiasu", "error", err.Error())
