44542fef0473acd7cd8c9555c6bdeefa9f3dd9a1
Ian Chiles <fortytw2@gmail.com>
2016-11-24 16:53:42 +0100

    include linting as part of tests (#19)

---
+++ lint_test.go
@@ -0,0 +1,19 @@
+package kiasu
+
+import (
+	"testing"
+
+	"github.com/surullabs/lint"
+)
+
+func TestLint(t *testing.T) {
+	// Run default linters
+	err := lint.Default.Check("./...")
+
+	// Ignore lint errors from auto-generated files
+	err = lint.Skip(err, lint.RegexpMatch(`internal/`, `_string\.go`, `\.pb\.go`))
+
+	if err != nil {
+		t.Fatalf("lint failures: %v\n", err)
+	}
+}
