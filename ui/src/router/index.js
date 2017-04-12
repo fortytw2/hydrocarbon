26b28ad28e2dac3c67bb2c5eb33832e34a77556c
Ian Chiles <fortytw2@gmail.com>
2017-04-12 20:18:00 +0200

    add vuex

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 
 Vue.use(Router)
+Vue.use(Vuex)
 
 export default new Router({
   routes: [
