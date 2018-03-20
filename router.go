3fb2cb1aa95ab762294d00d976e1abb25108bf18
fortytw2 <fortytw2@gmail.com>
2018-03-19 21:08:52 -0500

    handle panicking routes, thanks @docmerlin

--- router.go
+++ router.go
@@ -2,6 +2,7 @@ package hydrocarbon
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -18,6 +19,12 @@ import (
 type ErrorHandler func(w http.ResponseWriter, r *http.Request) error
 
 func (eh ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if r := recover(); r != nil {
+			writeErr(w, fmt.Errorf("%v", r))
+		}
+	}()
+
 	err := eh(w, r)
 	if err != nil {
 		writeErr(w, err)
